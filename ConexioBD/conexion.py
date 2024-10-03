a4d056840896416ea12c15bfb87944f039fb5dc8
Marcelo Portillo <mportillo79@gmail.com>
2024-10-03 19:56:19 -0300

    Se agrega el punto de acceso para gases

--- ConexioBD/conexion.py
+++ ConexioBD/conexion.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configuración de la base de datos MySQL
 db_config = {
     'host': os.getenv("HOST"),
-    'user': os.getenv("USERNAME"),
+    'user': os.getenv("USER"),
     'password': os.getenv("PASSWORD"),
     'database': os.getenv("DB")
 }
@@ -36,6 +36,43 @@ def connect_to_db():
 @app.route('/')
 def home():
     return "<p>Bienvenido a la API de Gases</p>"
+
+
+# MARCELO
+# ----------
+@app.route('/api/gases/data', methods=['POST'])
+def receive_data():
+    if request.is_json:
+        data = request.get_json()
+
+        # Ejemplo: Insertar los datos recibidos en una tabla MySQL
+        try:
+            connection = connect_to_db()
+            if connection is None:
+                return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+
+            cursor = connection.cursor()
+
+            # Supongamos que 'data' tiene un campo 'name' y 'value'
+            sql = "INSERT INTO  gases (escala_ppm) VALUES (%s)"
+            values = (data['medicion'])
+
+            cursor.execute(sql, values)
+            connection.commit()
+
+            return jsonify({"message": "Datos insertados correctamente"}), 200
+
+        except Error as e:
+            return jsonify({"error": str(e)}), 500
+
+        finally:
+            if connection and connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    else:
+        return jsonify({"error": "El formato de los datos no es JSON"}), 400
+# ----------
 
 
 @app.route('/saluda')
