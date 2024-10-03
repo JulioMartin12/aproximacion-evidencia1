1be643de45bfa3f3294a16f10742e032815cb6b8
Marcelo Portillo <mportillo79@gmail.com>
2024-10-03 19:20:18 -0300

    test

--- ConexioBD/conexion.py
+++ ConexioBD/conexion.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import mysql.connector
 from mysql.connector import Error
 import os
-
+# ----
 app = Flask(__name__)
 
 # Configuración de la base de datos MySQL
@@ -14,6 +14,8 @@ db_config = {
 }
 
 # Función para conectarse a MySQL
+
+
 def connect_to_db():
     try:
         connection = mysql.connector.connect(
@@ -29,13 +31,17 @@ def connect_to_db():
         return None
 
 # Ruta para la raíz
+
+
 @app.route('/')
 def home():
     return "<p>Bienvenido a la API de Gases</p>"
 
+
 @app.route('/saluda')
 def hello_world():
     return "<p>Hola perros</p>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
