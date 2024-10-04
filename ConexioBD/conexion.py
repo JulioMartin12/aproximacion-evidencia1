b9589e4d26164c6ba5fce1ce6847e1d1313b4063
Marcelo Portillo <mportillo79@gmail.com>
2024-10-03 22:26:33 -0300

    se agregan los point de sonido y movimiento

--- ConexioBD/conexion.py
+++ ConexioBD/conexion.py
@@ -39,8 +39,8 @@ def home():
 
 
 # MARCELO
-# ----------
-@app.route('/api/gases/data', methods=['POST'])
+# ------------------------------------------------------------------------------------------
+@app.route('/api/gas/data', methods=['POST'])
 def receive_data():
     if request.is_json:
         data = request.get_json()
@@ -72,7 +72,89 @@ def receive_data():
 
     else:
         return jsonify({"error": "El formato de los datos no es JSON"}), 400
-# ----------
+# ------------------------------------------------------------------------------------------
+
+
+# GUS
+# ------------------------------------------------------------------------------------------
+@app.route('/api/movimiento/data', methods=['POST'])
+def receive_data():
+    if request.is_json:
+        data = request.get_json()
+
+        # Ejemplo: Insertar los datos recibidos en una tabla MySQL
+        try:
+            connection = connect_to_db()
+            if connection is None:
+                return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+
+            cursor = connection.cursor()
+
+            # Supongamos que 'data' tiene un campo 'name' y 'value'
+            sql = "INSERT INTO  sensor_movimiento (escala_ppm) VALUES (%s)"
+            values = (data['medicion'])
+
+            cursor.execute(sql, values)
+            connection.commit()
+
+            return jsonify({"message": "Datos insertados correctamente"}), 200
+
+        except Error as e:
+            return jsonify({"error": str(e)}), 500
+
+        finally:
+            if connection and connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    else:
+        return jsonify({"error": "El formato de los datos no es JSON"}), 400
+# ------------------------------------------------------------------------------------------
+
+# JULIO
+# ------------------------------------------------------------------------------------------
+
+
+@app.route('/api/sonido/data', methods=['POST'])
+def receive_data():
+    if request.is_json:
+        data = request.get_json()
+
+        # Ejemplo: Insertar los datos recibidos en una tabla MySQL
+        try:
+            connection = connect_to_db()
+            if connection is None:
+                return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+
+            cursor = connection.cursor()
+
+            # Supongamos que 'data' tiene un campo 'name' y 'value'
+            sql = "INSERT INTO  monitor_sonido (escala_ppm) VALUES (%s)"
+            values = (data['medicion'])
+
+            cursor.execute(sql, values)
+            connection.commit()
+
+            return jsonify({"message": "Datos insertados correctamente"}), 200
+
+        except Error as e:
+            return jsonify({"error": str(e)}), 500
+
+        finally:
+            if connection and connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    else:
+        return jsonify({"error": "El formato de los datos no es JSON"}), 400
+# ------------------------------------------------------------------------------------------
+
+# aca deberiamos mostrar los datos guardados
+
+
+@app.route('/reporte')
+def reporte():
+    return "<p>Alto Reporte</p>"
 
 
 @app.route('/saluda')
