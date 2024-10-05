8d7104de03e4009b0fc1d4fcbef57ed1d8a1e61e
Gustavo Olmos <programacinwebolmos@gmail.com>
2024-10-05 02:54:58 -0300

    creacion de metodo post sensor_movimiento

--- ConexioBD/conexion.py
+++ ConexioBD/conexion.py
@@ -80,7 +80,38 @@ def receive_data_gas():
 
 # GUS
 # ------------------------------------------------------------------------------------------
+@app.route('/api/sensor_movimiento', methods=['POST'])
+def receive_data_sensor_movimiento():
+    if request.is_json:
+        data = request.get_json()
 
+        # Ejemplo: Insertar los datos recibidos en una tabla MySQL
+        try:
+            connection = connect_to_db()
+            if connection is None:
+                return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+
+            cursor = connection.cursor()
+
+            # Supongamos que 'data' tiene un campo 'name' y 'value'
+            sql = "INSERT INTO  sensor_movimiento (fecha,hora,mensaje) VALUES (%s,%s,%s)"
+            values = (data['fecha'], data['hora'], data['mensaje'])
+
+            cursor.execute(sql, values)
+            connection.commit()
+
+            return jsonify({"message": "Datos insertados correctamente"}), 200
+
+        except Error as e:
+            return jsonify({"error": str(e)}), 500
+
+        finally:
+            if connection and connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    else:
+        return jsonify({"error": "El formato de los datos no es JSON"}), 400
 # ------------------------------------------------------------------------------------------
 
 # JULIO
